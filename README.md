c857eb27f4186bc18aebfe3c3abceb577ec73bbe
Walison Ferreira <walisonferreiraa@gmail.com>
2022-02-20 14:32:51 -0300

    queue with multiple workers

--- README.md
+++ README.md
@@ -13,3 +13,18 @@ $> go run send_receive/receive/receive.go
 2022/02/20 14:13:01 [*] Waiting for messages. To exit press CTRL+C
 2022/02/20 14:13:01 Received a message: Hello World!
 ```
+
+### Worker queue
+
+```bash
+$> go run worker_queues/new_task/new_task.go
+
+2022/02/20 14:25:07 [X] Sent hello
+```
+```bash
+$> go run worker_queues/worker/worker.go
+
+2022/02/20 14:25:38 [*] Waiting for messages. To exit press Ctrl+C
+2022/02/20 14:25:38 Received a message: hello
+2022/02/20 14:25:38 Done
+```
---
+++ worker_queues/worker/worker.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bytes"
+	"github.com/streadway/amqp"
+	"log"
+	"time"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"task_queue",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	err = ch.Qos(
+		1,
+		0,
+		false,
+	)
+	failOnError(err, "Failed to set QoS")
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+			dotCount := bytes.Count(d.Body, []byte("."))
+			t := time.Duration(dotCount)
+			time.Sleep(t * time.Second)
+			log.Printf("Done")
+			d.Ack(false)
+		}
+	}()
+
+	log.Printf("[*] Waiting for messages. To exit press Ctrl+C")
+	<-forever
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
