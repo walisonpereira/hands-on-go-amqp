b10b2ec81708ef2d9690c6ca4f87a18cc8f3e54e
Walison Ferreira <walisonferreiraa@gmail.com>
2022-02-20 14:07:41 -0300

    simple producer and consumer

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/walisonpereira/hands-on-go-amqp
+
+go 1.17
+
+require github.com/streadway/amqp v1.0.0
---
+++ send_receive/send/send.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/streadway/amqp"
+	"log"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	body := "Hello World!"
+	err = ch.Publish(
+		"",
+		q.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf("[X] Sent %s\n", body)
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
